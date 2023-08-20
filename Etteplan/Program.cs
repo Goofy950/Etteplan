b7cccba41e94fbd713396a170b4ee7e910d1ed22
Goofy950 <92301535+Goofy950@users.noreply.github.com>
2023-08-20 17:38:14 +0200

    Initial commit

---
+++ Etteplan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+//Path till Xml filen @"C:\....
+var xml = XDocument.Load(@"C:\");
+var query = from c in xml.Descendants("trans-unit")
+            where (string)c.Attribute("id") == "42007"
+            select c.Element("target").Value;
+            
+
+//Path till vart txt filen skall sparas C:\.....\Text.txt <-- annars skapas inte filen med StreamWriter, namnet kvittar
+var report = @"C:\";
+StreamWriter sw = new StreamWriter(report);
+sw.WriteLine(query.FirstOrDefault());
+sw.Close();
+Console.WriteLine("saving");
+Console.ReadLine();
+
+
